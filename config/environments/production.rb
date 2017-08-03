c948dc01656607c23655366e49f046087fb1a90e
Eric Post <erickpost@gmail.com>
2017-08-02 17:32:03 -0700

    updated some sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,12 +89,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'http://eric-post.ddns.net',
+    :domain => 'ddns.net',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
     :enable_starttls_auto => true
-  }
-end
+    }
+  end
